1d1f6a392b5440a7ce02dfd2a14e8f83a0a30900
joeyclapton <joeyclapton42@gmail.com>
2023-01-17 17:01:55 -0300

    feat: create books component

--- concepts/src/app/app.module.ts
+++ concepts/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BooksModule } from './books/books.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, BooksModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ concepts/src/app/books/components/books/books.template.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'books',
+  templateUrl: './books.template.html',
+  styleUrls: ['./books.template.scss'],
+})
+export class BooksComponent {}
