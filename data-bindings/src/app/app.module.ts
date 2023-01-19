56bf2f707faca5ca3eb7f81e798535cf4f62fa0a
joeyclapton <joeyclapton42@gmail.com>
2023-01-19 15:07:33 -0300

    feat: create alert component

--- data-bindings/src/app/app.module.ts
+++ data-bindings/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AlertComponent } from './alert/alert.component';
 import { AppComponent } from './app.component';
 import { RandomImage } from './random-image/random-image.component';
 
 @NgModule({
-  declarations: [AppComponent, RandomImage],
+  declarations: [AppComponent, RandomImage, AlertComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
