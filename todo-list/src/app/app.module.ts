6b3ff1773f24a7fce3e4985ed9a6963e0fc4934a
joeyclapton <joeyclapton42@gmail.com>
2023-01-26 18:34:21 -0300

    feat: add  todos component

--- todo-list/src/app/app.module.ts
+++ todo-list/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NewTodoComponent } from './new-todo/new-todo.component';
+import { TodoComponent } from './todo/todo.component';
+import { TodosComponent } from './todos/todos.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, NewTodoComponent, TodoComponent, TodosComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
