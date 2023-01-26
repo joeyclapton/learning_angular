47778f51ef4e4d21f68c75162b7e79686c9cf8c1
joeyclapton <joeyclapton42@gmail.com>
2023-01-26 18:12:11 -0300

    feat: initial commit todo-list project

--- todo-list/src/app/app.component.spec.ts
+++ todo-list/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'data-bindings-project'`, () => {
+  it(`should have as title 'todo-list'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('data-bindings-project');
+    expect(app.title).toEqual('todo-list');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('data-bindings-project app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('todo-list app is running!');
   });
 });
--- todo-list/src/app/app.component.ts
+++ todo-list/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'data-bindings-project';
+  title = 'todo-list';
 }
