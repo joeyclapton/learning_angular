72b01c796688ed15ffdb85e3191f1fd28ecee19c
joeyclapton <joeyclapton42@gmail.com>
2023-02-01 10:37:58 -0300

    feat: add directive bg

--- directives/src/app/shared/directivas/background.directive.ts
+++ directives/src/app/shared/directivas/background.directive.ts
@@ -1,10 +1,20 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
 
 @Directive({
-  selector: '[appBackground]'
+  selector: '[appBackground]',
 })
 export class BackgroundDirective {
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {
+    this.elementRef = elementRef;
+    this.renderer = renderer;
 
-  constructor() { }
+    // Note usual(XSS attack can provide)
+    // this.elementRef.nativeElement.style.backgroundColor = this.color;
 
+    this.renderer.setStyle(
+      this.elementRef.nativeElement,
+      'backgroundColor',
+      'cyan'
+    );
+  }
 }
