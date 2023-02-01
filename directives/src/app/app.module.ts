87b3dce1187736d8c2314f0071cddca0149871f0
joeyclapton <joeyclapton42@gmail.com>
2023-02-01 09:33:46 -0300

    feat: create post component

--- directives/src/app/app.module.ts
+++ directives/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { StatusComponent } from './status/status.component';
 import { UserInfoComponent } from './user-info/user-info.component';
 import { ContentListComponent } from './content-list/content-list.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StatusComponent,
     UserInfoComponent,
-    ContentListComponent
+    ContentListComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule
