b6fb11896698204ada68d68647316d94bff3efc1
joeyclapton <joeyclapton42@gmail.com>
2023-03-30 16:36:20 -0300

    feat: create servers component

---
+++ 01_hello_world/src/app/servers/servers.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.sass']
+})
+export class ServersComponent {
+
+}
