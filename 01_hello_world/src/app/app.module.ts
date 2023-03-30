f885be0b41749b93aff9367b3c1c5d8d5dfdbbb4
joeyclapton <joeyclapton42@gmail.com>
2023-03-30 16:59:33 -0300

    feat: create alert success compoentn

--- 01_hello_world/src/app/app.module.ts
+++ 01_hello_world/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { AlertSuccessComponent } from './alert-success/alert-success.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent],
+  declarations: [AppComponent, ServerComponent, ServersComponent, AlertSuccessComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
