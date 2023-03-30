3f5bf38720f6879ae1796cc2ce3c8ef266c9e620
joeyclapton <joeyclapton42@gmail.com>
2023-03-30 16:54:26 -0300

    feat: some updates

--- 01_hello_world/src/app/servers/servers.component.ts
+++ 01_hello_world/src/app/servers/servers.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.sass']
+  styleUrls: ['./servers.component.sass'],
 })
-export class ServersComponent {
-
-}
+export class ServersComponent {}
