7f39f41f850554356281d0ab4ea2db61c3c7282e
joeyclapton <joeyclapton42@gmail.com>
2023-05-04 17:04:55 -0300

    refactor: clean project

--- 01_hello_world/src/app/servers/servers.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.sass'],
-})
-export class ServersComponent {}
--- concepts/services/src/app/components/courses/courses.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class CoursesService {
-    getCursos(): Array<string> {
-        return ['Angular', 'Node', 'Flutter']
-    }
-}
