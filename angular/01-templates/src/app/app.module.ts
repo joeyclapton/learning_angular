6fff0d18419d1ac6bc361b17a605bac7ad5db360
joeyclapton <joeyclapton42@gmail.com>
2023-08-03 14:18:54 -0300

    feat: create random color component

--- angular/01-templates/src/app/app.module.ts
+++ angular/01-templates/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MessageComponent } from './components/message/message.component';
 import { LogoComponent } from './components/logo/logo.component';
+import { RandomColorComponent } from './components/random-color/random-color.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MessageComponent,
-    LogoComponent
+    LogoComponent,
+    RandomColorComponent
   ],
   imports: [
     BrowserModule
---
+++ angular/01-templates/src/app/components/random-color/random-color.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-random-color",
+    templateUrl: './random-color.component.html',
+    styleUrls: ['./random-color.component.sass']
+})
+export class RandomColorComponent {
+    isRed = true;
+}
